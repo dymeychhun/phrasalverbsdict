8f0901977bcd8645a701eb96881bf040d9fae728
Dymey Chhun <93627813+dymeychhun@users.noreply.github.com>
2024-04-16 21:20:24 +0700

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+  $("#search").focus();
+  // get data
+  $.get(
+    "db/dictdb.json",
+    function (data) {
+      // console.log(data);
+      data.forEach((item) => {
+        $("#app").append(
+          `<li class="list-group-item list-group-item-action selected" data-def="${item.definitions}">${item.words}</li>`
+        );
+      });
+    },
+    "json"
+  );
+
+  // select word
+  $(document).on("click", ".selected", function () {
+    let word = $(this).text();
+    let def = $(this).data("def");
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+    // console.log(def);
+    $("#def").html(
+      "<h3 class='text-danger'>" +
+        word +
+        "</h3>" +
+        "<h4 class='text-primary'>" +
+        def +
+        "</h4>"
+    );
+  });
+
+  // filter word
+  $("#search").on("keyup", function () {
+    let value = $(this).val().toLowerCase();
+    $("ul li").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
+});
